a998051a51d8586081a2c71430c0faac9d4cdc2d
Md Rasel Ahmed <61163868+ra-sel@users.noreply.github.com>
2020-04-12 01:32:25 +0600

    Add files via upload

---
+++ precache-manifest.18dd72a7b9f416672bec5682ed0da185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fff4603bfb22fa443bd8af036a9079f",
+    "url": "/react_library_management/index.html"
+  },
+  {
+    "revision": "62a6b0c7b4da01a921ce",
+    "url": "/react_library_management/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0b97d173bfb8d962f23e",
+    "url": "/react_library_management/static/css/main.5d1dd0a9.chunk.css"
+  },
+  {
+    "revision": "62a6b0c7b4da01a921ce",
+    "url": "/react_library_management/static/js/2.d8f4b5bc.chunk.js"
+  },
+  {
+    "revision": "0b97d173bfb8d962f23e",
+    "url": "/react_library_management/static/js/main.82dd3951.chunk.js"
+  },
+  {
+    "revision": "f32a02ce293580399e24",
+    "url": "/react_library_management/static/js/runtime-main.b2a2a5ac.js"
+  }
+]);
